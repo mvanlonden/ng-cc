3a42a777c7339c54962a30f9e42c6213c5d43134
mvanlonden <melvanlonden@gmail.com>
2014-11-11 23:38:17 -0500

    template in place

--- app/scripts/directives/ccinput.js
+++ app/scripts/directives/ccinput.js
@@ -9,7 +9,7 @@
 angular.module('ng-cc')
   .directive('ccInput', function () {
     return {
-      templateUrl: './../../views/ccinput.html',
+      template: '<div class="ccContainer"> <cc-number number="number" on-complete="moveTo(&quot;date&quot;)" ng-show="currentField === &quot;number&quot;" update="currentField"></cc-number> <input type="text" ng-click="currentField = &quot;number&quot;" ng-model="lastFour" ng-hide="currentField === &quot;number&quot;"> <cc-date on-complete="moveTo(&quot;zip&quot;)" update="currentField" ng-hide="currentField === &quot;number&quot;"></cc-date> <cc-zip on-complete="moveTo(&quot;cvc&quot;)" update="currentField" ng-hide="currentField === &quot;number&quot;"></cc-zip> <cc-cvc on-complete="validateCC()" update="currentField" ng-hide="currentField === &quot;number&quot;"></cc-cvc> </div>',
       restrict: 'E',
       replace: true,
       scope: {},
